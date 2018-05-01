5c72337b47d50f5dc2a023d2f7a22222dcc44233
dennis monteiro <dnss31@hotmail.com>
2018-04-30 22:59:39 -0300

    Add project files.

---
+++ src/SGC.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.ApplicationCore.Entity
+{
+    class Cliente
+    {
+		public Cliente()
+		{
+
+			
+		}
+
+		public int ClienteID { get; set; }
+
+		public string Nome { get; set; }
+
+		public string CPF { get; set; }
+	}
+}
